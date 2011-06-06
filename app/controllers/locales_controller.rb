9d58fcf9d173af2d6dba6cbb3270c35ee5343176
Phil <phil@branch14.org>
2011-06-06 14:35:18 +0200

    limit translations shown, filters must at least have 3 chars, display hit count

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -2,10 +2,16 @@ class LocalesController < InheritedResources::Base
 
   belongs_to :project
 
+  before_filter :load_translations, :only => :show
+
   private
 
-  def resource 
-    @locale ||= Locale.where(:id => params[:id]).includes(:translations => :token).first
+  #def resource 
+  #  @locale ||= Locale.where(:id => params[:id]).first
+  #end
+
+  def load_translations
+    @translations = resource.translations.limit(10).order('hits DESC').includes(:token)
   end
 
 end
--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -5,7 +5,7 @@ class TranslationsController < InheritedResources::Base
   private
 
   def collection
-    @translations ||= Translation.page(params[:page]).order('content')
+    @translations ||= Translation.order('hits DESC').includes(:token)
     if params[:filter]
       @translations = @translations.where('content LIKE ?', "%#{params[:filter]}%").
         where(:locale_id => params[:locale_id])
