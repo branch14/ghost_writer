827a908f691a758593889b6e231b648a92bc564c
phil <phil@branch14.org>
2011-09-02 13:43:06 +0200

    change session store key for rails 3.1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ProjectZero::Application.config.session_store :cookie_store, :key => '_project_zero_session'
+ProjectZero::Application.config.session_store :cookie_store, :key => '_ghostwriter_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
